4d56d97f6f77d61c379e6b80cc68e4a0ab3e2573
Peter Gill <peter.gill@townsuite.com>
2023-09-18 14:16:02 -0230

    Fix sql server nondestructive dequeue
    Missed in the last commit

--- scripts/sql-server/dbo.WorkQueue_Dequeue_NonDestructive.sql
+++ scripts/sql-server/dbo.WorkQueue_Dequeue_NonDestructive.sql
@@ -12,15 +12,18 @@ CREATE PROCEDURE [dbo].[WorkQueue_Dequeue_NonDestructive]
 AS
 BEGIN
 
+    DECLARE @the_payload nvarchar(max);
     UPDATE TOP(1) dbo.workqueue
-    SET timeprocessedutc = GETUTCDATE(), @p_payload=payload
+    SET timeprocessedutc = GETUTCDATE(), @the_payload=payload
     WHERE id = (
     SELECT id
     FROM workqueue WITH (ROWLOCK, UPDLOCK, READPAST)
-        WHERE channel = @p_channel
+        WHERE channel = @p_channel and timeprocessedutc is null
     ORDER BY id
     OFFSET @p_offset ROWS
     FETCH NEXT 1 ROWS ONLY);
+
+    SELECT @p_payload = @the_payload;
 
 END
 
